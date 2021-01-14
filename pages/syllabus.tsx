5c2bfb024c501588d8bd151ec9204290dbc1cfb2
Ye Joo Park <subwaymatch@gmail.com>
2021-01-13 20:41:34 -0600

    Prettier plugin test

--- pages/syllabus.tsx
+++ pages/syllabus.tsx
@@ -1,23 +1,30 @@
-
-import Layout from 'components/Layout';
-import Header from 'components/Header';
-import { Container, Row, Col } from 'react-bootstrap';
-import Link from 'next/link';
+import Layout from "components/Layout";
+import Header from "components/Header";
+import { Container, Row, Col } from "react-bootstrap";
+import Link from "next/link";
 
 export default function SyllabusPage() {
-    return (
-        <Layout>
-            <Header />
+  return (
+    <Layout>
+      <Header />
 
-            <main>
-                <Container>
-                    <Row>
-                        <Col>
-                            <h1>Introduction to Data Analytics Applications in Business</h1>
-                        </Col>
-                    </Row>
-                </Container>
-            </main>
-        </Layout>
-    );
+      <main>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="pageTitle">
+                Introduction to Data Analytics Applications in Business
+              </h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <h2 className="sectionTitle">Course Information</h2>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </Layout>
+  );
 }
