14f59c0dd0d2f96fada728bc00a9d64525ea8f49
Ye Joo Park <subwaymatch@gmail.com>
2021-01-14 07:30:23 -0600

    Mobile layout for grade breakdown

--- components/pages/syllabus/GradeBreakdown.tsx
+++ components/pages/syllabus/GradeBreakdown.tsx
@@ -3,24 +3,24 @@ import styles from "./GradeBreakdown.module.scss";
 import clsx from "clsx";
 
 const GradedItem = ({ item, points, percentage, notes }) => (
-  <div className={styles.gradedItem}>
+  <div className={styles.itemRow}>
     <Row>
       <Col sm={4} xs={6}>
-        <span className={clsx(styles.columnCell)}>{item}</span>
+        <div className={clsx(styles.columnCell)}>{item}</div>
       </Col>
 
       <Col sm={2} xs={3}>
-        <span className={clsx(styles.columnCell)}>{points}</span>
+        <div className={clsx(styles.columnCell)}>{points}</div>
       </Col>
 
       <Col sm={2} xs={3}>
-        <span className={clsx(styles.columnCell, styles.purple)}>
+        <div className={clsx(styles.columnCell, styles.purple)}>
           {percentage}
-        </span>
+        </div>
       </Col>
 
       <Col sm={4} xs={12}>
-        <span className={clsx(styles.columnCell, styles.note)}>{notes}</span>
+        <div className={clsx(styles.columnCell, styles.note)}>{notes}</div>
       </Col>
     </Row>
   </div>
@@ -41,48 +41,63 @@ export default function GradeBreakdown() {
 
         <Row className={styles.headerRow}>
           <Col sm={4} xs={6}>
-            <span className={clsx(styles.columnCell, styles.columnHeader)}>
+            <div className={clsx(styles.columnCell, styles.columnHeader)}>
               Item
-            </span>
+            </div>
           </Col>
 
           <Col sm={2} xs={3}>
-            <span className={clsx(styles.columnCell, styles.columnHeader)}>
+            <div className={clsx(styles.columnCell, styles.columnHeader)}>
               Points
-            </span>
+            </div>
           </Col>
 
           <Col sm={2} xs={3}>
-            <span
+            <div
               className={clsx(
                 styles.columnCell,
                 styles.columnHeader,
                 styles.purple
               )}
             >
-              Percentage
-            </span>
+              <span className="d-sm-none">%</span>
+              <span className="d-none d-sm-inline">Percentage</span>
+            </div>
           </Col>
 
           <Col sm={4} className="d-none d-sm-block">
-            <span className={clsx(styles.columnCell, styles.columnHeader)}>
+            <div className={clsx(styles.columnCell, styles.columnHeader)}>
               Notes
-            </span>
+            </div>
           </Col>
         </Row>
 
         <GradedItem
           item="In-class Exercise"
           points={200}
-          percentage={0.2}
+          percentage="20%"
           notes="Every class"
         />
 
         <GradedItem
           item="After-class Exercise"
           points={100}
-          percentage={0.1}
+          percentage="10%"
           notes="20 Exercises"
+        />
+
+        <GradedItem
+          item="Case Studies"
+          points={320}
+          percentage="32%"
+          notes="8 Cases"
+        />
+
+        <GradedItem
+          item="Extra Credit"
+          points={10}
+          percentage="1%"
+          notes="Accountancy Research Lab"
         />
       </Container>
     </section>
