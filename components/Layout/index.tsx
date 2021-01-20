980d68ab59ce357835ecd7d1a622112008c88a70
Ye Joo Park <subwaymatch@gmail.com>
2021-01-20 11:44:00 -0600

    Add U of I mark favicon

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,17 +1,39 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 type PropTypes = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export default function Layout({ children }: PropTypes) {
-    return (
-        <>
-            <Head>
-                <title>BDI 475</title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>BDI 475</title>
 
-            {children}
-        </>
-    )
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00ff88" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#00ff88" />
+      </Head>
+
+      {children}
+    </>
+  );
 }
