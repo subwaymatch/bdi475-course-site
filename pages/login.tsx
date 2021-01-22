edff869c0ae3f7a42b6e1cd355eda24c522edac9
Ye Joo Park <subwaymatch@gmail.com>
2021-01-22 12:08:10 -0600

    Handle email link login in login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "components/Layout";
 import Header from "components/Header";
 import { Container, Row, Col } from "react-bootstrap";
 import { RiSendPlaneLine } from "react-icons/ri";
 import styles from "styles/pages/login.module.scss";
 import clsx from "clsx";
-import { firebaseClient } from "firebase/firebaseClient";
+import { firebaseClient as firebase } from "firebase/firebaseClient";
 
 export default function LoginPage() {
   const [netId, setNetId] = useState("");
+
+  useEffect(() => {
+    console.log(location);
+
+    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+      console.log("Sign in!");
+
+      let email = window.localStorage.getItem("emailForSignIn");
+
+      if (!email) {
+        email = window.prompt("Please provide your email for confirmation");
+      }
+
+      firebase
+        .auth()
+        .signInWithEmailLink(email, window.location.href)
+        .then((result) => {
+          window.localStorage.removeItem("emailForSignIn");
+          console.log("Sign in complete");
+          console.log(result);
+        })
+        .catch((err) => {
+          console.error("Error signing in through email link");
+          console.log(err.errorCode);
+          console.log(err.errorMessage);
+        });
+    }
+  }, []);
+
+  const sendSignInLink = async (email: string) => {
+    const actionCodeSettings = {
+      // URL you want to redirect back to. The domain (www.example.com) for this
+      // URL must be in the authorized domains list in the Firebase Console.
+      url: location.href,
+      // This must be true.
+      handleCodeInApp: true,
+    };
+
+    try {
+      await firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings);
+      window.localStorage.setItem("emailForSignIn", email);
+      console.log(`Successfully sent an email sign in link to ${email}`);
+    } catch (err) {
+      const errorCode = err.code;
+      const errorMessage = err.message;
+
+      console.error("Error sending authentication email");
+      console.log(errorCode);
+      console.log(errorMessage);
+    }
+  };
 
   return (
     <Layout>
@@ -61,7 +112,13 @@ export default function LoginPage() {
 
           <Row className={clsx(styles.submitControls, "align-items-center")}>
             <Col md={4} xs={12}>
-              <a className="greenButton">
+              <a
+                className="greenButton"
+                onClick={(e) => {
+                  e.preventDefault();
+                  sendSignInLink(netId + "@illinois.edu");
+                }}
+              >
                 <span className={styles.label}>Email Me</span>
                 <RiSendPlaneLine className={styles.reactIcon} />
               </a>
