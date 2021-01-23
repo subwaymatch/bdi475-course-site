bb5c94413bbe6424549a9f1fc7e44f3bcf4d4a0a
Ye Joo Park <subwaymatch@gmail.com>
2021-01-23 02:34:19 -0600

    Add toast and set up customized colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import type { AppProps } from "next/app";
 import AuthProvider from "firebase/AuthProvider";
 import "styles/globals.scss";
 
+// Toastify default & custom styles
+import "react-toastify/dist/ReactToastify.css";
+import "styles/toastify.custom.scss";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
