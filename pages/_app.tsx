34114423b058b6a26055f9b9e86c7bb3f6a49d2e
Ye Joo Park <subwaymatch@gmail.com>
2021-01-24 01:31:17 -0600

    Move ToastContainer to _app.tsx to keep toast messages after navigations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import AuthProvider from "firebase/AuthProvider";
+import { ToastContainer } from "react-toastify";
 import "styles/globals.scss";
 
 // Toastify default & custom styles
@@ -9,6 +10,17 @@ import "styles/toastify.custom.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Component {...pageProps} />
     </AuthProvider>
   );
