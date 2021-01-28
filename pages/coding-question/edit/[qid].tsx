05c99057fa45e6d8c3842b63d5b54fe902278a55
Ye Joo Park <subwaymatch@gmail.com>
2021-01-28 11:42:49 -0600

    Connect Firestore codingQuestions collection in EditCodingQuestionPage

--- pages/coding-question/edit/[qid].tsx
+++ pages/coding-question/edit/[qid].tsx
@@ -1,28 +1,29 @@
+import { useState, useEffect } from "react";
 import QuestionEditor from "components/coding-question/QuestionEditor";
 import Layout from "components/Layout";
+import { useRouter } from "next/router";
+import { useFirestoreDocData, useFirestore } from "reactfire";
 
 export default function EditCodingQuestionPage(props) {
+  const router = useRouter();
+  const { qid } = router.query;
+
+  const [title, setTitle] = useState("");
+  const [textMarkdown, setTextMarkdown] = useState("");
+  const [starterCode, setStarterCode] = useState("");
+  const [solutionCode, setSolutionCode] = useState("");
+  const [testCode, setTestCode] = useState("");
+
+  const docRef = useFirestore()
+    .collection("codingQuestions")
+    .doc(qid as string);
+
+  const { status, data } = useFirestoreDocData(docRef);
+
   console.log(props);
   return (
     <Layout excludeHeader={true}>
       <QuestionEditor />
     </Layout>
   );
-}
-
-export async function getServerSideProps(context) {
-  console.log(context.params);
-
-  // Fetch question data from the server
-  const data = {
-    // id: questionId,
-    initialTitle: "",
-    initialStarter: "# Starter code",
-    initialSolution: "# Solution",
-    initialTestCode: "# Test Cases",
-  };
-
-  return {
-    props: data,
-  };
 }
