c539fe4adbadb75cab7bd4d29dad3fa76879bd92
Ye Joo Park <subwaymatch@gmail.com>
2021-02-22 13:55:38 -0600

    Finish functions lecture note

--- pages/notes/functions.tsx
+++ pages/notes/functions.tsx
@@ -67,6 +67,103 @@ export default function FunctionPage() {
           <RecordedCodingQuestion qid="QDn300" className={styles.block} />
 
           <RecordedCodingQuestion qid="2O1csh" className={styles.block} />
+
+          <CenteredColumn className={styles.textBox}>
+            <h3>What are functions?</h3>
+
+            <span className="label green">Back to Algebra</span>
+
+            <p>
+              Let's forget about Python for a moment and go back a few years all
+              the way back to Algebra. Do you remember any expressions that
+              resemble the form of <code>f(x) = x + 3</code>?
+            </p>
+
+            <p>
+              <code>f(x) = x + 3</code> is a <em>function</em>. For any given{" "}
+              <code>x</code>, the function will add <code>3</code> to{" "}
+              <code>x</code> and return that value.
+            </p>
+
+            <ul>
+              <li>
+                <code>f(5) = 5 + 3</code>, returns <code>8</code>
+              </li>
+
+              <li>
+                <code>f(10) = 10 + 3</code>, returns <code>13</code>
+              </li>
+
+              <li>
+                <code>f(-3) = -3 + 3</code>, returns <code>0</code>
+              </li>
+            </ul>
+
+            <p>
+              In mathematical terms, a function takes one or more inputs and
+              produces an output. Our previous function{" "}
+              <code>f(x) = x + 3</code> can be broken down into:
+            </p>
+
+            <p>
+              <img
+                src="https://accy570-fa2020-course-site-assets.s3-us-west-2.amazonaws.com/images/function-breakdown-01.png"
+                alt="Function Breakdown"
+              />
+            </p>
+
+            <p>
+              <code>f</code> in front of the parentheses is an arbitrary name
+              used to represent a function. We can choose to rename our function
+              to <code>add_three</code>. This changes our function expression to{" "}
+              <code>add_three(x) = x + 3</code>.
+            </p>
+
+            <p>
+              A function can also take more than one input (e.g.,{" "}
+              <code>add(a, b) = a + b</code>).
+            </p>
+
+            <p>
+              In programming, an inputs is referred as an <em>argument</em>, and
+              an output is referred as a <em>return value</em>.
+            </p>
+
+            <p>
+              <img
+                src="https://accy570-fa2020-course-site-assets.s3-us-west-2.amazonaws.com/images/function-diagram-01.png"
+                alt="Function Diagram"
+              />
+            </p>
+          </CenteredColumn>
+
+          <RecordedCodingQuestion qid="MSYxAM" className={styles.block} />
+
+          <RecordedCodingQuestion qid="3Ubagd" className={styles.block} />
+
+          <RecordedCodingQuestion qid="JnTExm" className={styles.block} />
+
+          <RecordedCodingQuestion qid="DmaGpd" className={styles.block} />
+
+          <RecordedCodingQuestion qid="ThsXV0" className={styles.block} />
+
+          <RecordedCodingQuestion qid="N8pjpw" className={styles.block} />
+
+          <RecordedCodingQuestion qid="w4mn5F" className={styles.block} />
+
+          <RecordedCodingQuestion qid="IOeA2s" className={styles.block} />
+
+          <RecordedCodingQuestion qid="OAS9vf" className={styles.block} />
+
+          <RecordedCodingQuestion qid="jR3Wxg" className={styles.block} />
+
+          <RecordedCodingQuestion qid="rr9KIl" className={styles.block} />
+
+          <RecordedCodingQuestion qid="6qiy24" className={styles.block} />
+
+          <RecordedCodingQuestion qid="1iHGVQ" className={styles.block} />
+
+          <RecordedCodingQuestion qid="MV0U9q" className={styles.block} />
         </Container>
       </main>
     </Layout>
