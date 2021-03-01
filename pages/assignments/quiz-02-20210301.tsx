53c495d3de7e65f6d530be9368f7fd7c9ef3b514
Ye Joo Park <subwaymatch@gmail.com>
2021-03-01 13:40:06 -0600

    Add quiz-02-hidden-url

---
+++ pages/assignments/quiz-02-20210301.tsx
@@ -0,0 +1,55 @@
+import Layout from "components/Layout";
+import Image from "next/image";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "styles/pages/notes/common.module.scss";
+import CenteredColumn from "components/common/CenteredColumn";
+import RecordedCodingQuestion from "components/common/RecordedCodingQuestion";
+
+export default function QuizPage() {
+  return (
+    <Layout>
+      <main className={styles.page}>
+        <Container>
+          <Row>
+            <Col>
+              <h2 className="sectionTitle grayBottomBorder">
+                Quiz 2
+                <span className="accent purple" />
+              </h2>
+            </Col>
+          </Row>
+
+          <CenteredColumn className={styles.textBox}>
+            <h3>Yeezy Sneakers</h3>
+
+            <span className="label purple">Price of Sneakers</span>
+            <p>
+              Have you ever heard of sneakers that cost over $1,000 in resale
+              markets? 🔥🔥 I was stunned when I randomly ran into this list in{" "}
+              <a href="https://www.statista.com/">Statista</a> (a data provider
+              service). How exciting! You'll work with a list of most popular{" "}
+              <span className="color-purple">Yeezy</span> sneakers.
+            </p>
+          </CenteredColumn>
+
+          <Row>
+            <Col>
+              <div className={styles.coverImage}>
+                <Image
+                  src="/images/case-studies/553675_v6_adidas_com.jpg"
+                  width={1600}
+                  height={512}
+                  alt=""
+                />
+              </div>
+            </Col>
+          </Row>
+
+          <RecordedCodingQuestion qid="xQ9xiU" className={styles.block} />
+
+          <RecordedCodingQuestion qid="eJleeD" className={styles.block} />
+        </Container>
+      </main>
+    </Layout>
+  );
+}
