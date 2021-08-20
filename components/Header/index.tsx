49b35f923d20bd1762299bc083eb640b5a33b95c
Ye Joo Park <subwaymatch@gmail.com>
2021-08-20 18:40:08 -0500

    react-bootstrap v2 + bootstrap 5.1.0 build test

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -51,7 +51,10 @@ const HeaderDesktopMenu = () => {
 
   return (
     <Row className={clsx(styles.mainMenu, "align-items-center")}>
-      <Col>
+      <Col
+        className="align-items-center"
+        style={{ backgroundColor: "yellowgreen", height: "100%" }}
+      >
         <Link href="/">
           <Image src={logoImage} alt="BDI 475" width={140} height={36} />
         </Link>
