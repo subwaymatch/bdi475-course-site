415b21e620f0e31faf21a5f40d25dda5cca580e3
Ye Joo Park <subwaymatch@gmail.com>
2021-08-20 04:59:55 -0500

    Remove unused code and comments

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@ module.exports = {
   webpack: (config, { isServer, webpack }) => {
     console.log(`Webpack version: ${webpack.version}`);
 
-    /*
-      Warning message when working with WASM in dev
-      The configured output.hotUpdateMainFilename doesn't lead times.   
-      to unique filenames per runtime and HMR update differs beied updattween runtimes.
-      This might lead to incorrect runtime behavior of the appl.hotUpdatied update.                                              eady on h
-      To fix this, make sure to include [runtime] in the output.hotUpdateMainFilename option, or use the default config.
-    */
-    // config.output.hotUpdateMainFilename =
-    //   "static/webpack/[fullhash].[runtime].hot-update.json";
-
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
