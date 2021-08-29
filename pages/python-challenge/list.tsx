57e6e14bda864db3ae4af3d0105c43661f1586d1
Ye Joo Park <subwaymatch@gmail.com>
2021-08-28 21:21:07 -0500

    Fix page reloads on item fetch in list page

--- pages/python-challenge/list.tsx
+++ pages/python-challenge/list.tsx
@@ -36,14 +36,14 @@ export default function PythonChallengeListPage() {
       onDelete: async () => {
         if (
           window.confirm(
-            "Are you sure you want to delete this question? This cannot be undone."
+            "Are you sure you want to delete this challenge? This cannot be undone."
           )
         ) {
           // await collectionRef.doc(cid).delete();
 
           toast.info(
             <div>
-              Deleted question <code>{challengeData.id}</code>
+              Deleted challenge <code>{challengeData.id}</code>
             </div>
           );
         }
@@ -53,9 +53,14 @@ export default function PythonChallengeListPage() {
     return item;
   };
 
-  const loadPage = async () => {
-    setStatus("loading");
+  const initialize = async () => {
+    await calculatePageCount();
+    await loadPage();
+  };
 
+  const loadPage = async () => {
+    console.log(`loadPage currentPageIndex=${currentPageIndex}`);
+    setStatus("loading");
     const { data, error } = await supabaseClient
       .from<definitions["coding_challenges"]>("coding_challenges")
       .select()
@@ -73,71 +78,69 @@ export default function PythonChallengeListPage() {
   };
 
   useEffect(() => {
-    calculatePageCount();
+    initialize();
   }, []);
 
-  useEffect(() => {
-    loadPage();
-  }, [currentPageIndex]);
+  const handlePageClick = async (data) => {
+    console.log(`handlePageClick`);
+    console.log(data);
+    setCurrentPageIndex(data.selected);
+    await loadPage();
+  };
 
   return (
     <Layout>
-      {status === "loading" ? (
+      <main className={styles.page}>
         <Container>
           <Row>
-            <Col>Loading...</Col>
+            <Col>
+              <h2 className="sectionTitle grayBottomBorder">
+                Python Challenges
+                <span className="accent pink" />
+              </h2>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={4}>
+              <Link href="/python-challenge/new">
+                <a className={styles.createButton}>+ Create</a>
+              </Link>
+            </Col>
+
+            <Col md={4}>Page {currentPageIndex + 1}</Col>
+          </Row>
+
+          {status === "loading" ? (
+            <Row>
+              <Col>Loading...</Col>
+            </Row>
+          ) : (
+            <ChallengeList items={listItems} />
+          )}
+
+          <Row>
+            <Col md={12}>
+              <div className="paginationContainer">
+                <ReactPaginate
+                  pageCount={totalNumberOfPages}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={5}
+                  onPageChange={handlePageClick}
+                  containerClassName="pagination"
+                  activeClassName="active"
+                  pageClassName="page"
+                  previousLabel="←"
+                  previousClassName="previous"
+                  nextLabel="→"
+                  nextClassName="next"
+                  eventListener="onClick"
+                />
+              </div>
+            </Col>
           </Row>
         </Container>
-      ) : (
-        <main className={styles.page}>
-          <Container>
-            <Row>
-              <Col>
-                <h2 className="sectionTitle grayBottomBorder">
-                  Python Challenges
-                  <span className="accent pink" />
-                </h2>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col md={4}>
-                <Link href="/python-challenge/new">
-                  <a className={styles.createButton}>+ Create</a>
-                </Link>
-              </Col>
-
-              <Col md={4}>Page {currentPageIndex + 1}</Col>
-            </Row>
-
-            <ChallengeList items={listItems} />
-
-            <Row>
-              <Col md={12}>
-                <div className="paginationContainer">
-                  <ReactPaginate
-                    pageCount={totalNumberOfPages}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={(data) => {
-                      console.log(`onPageChange`);
-                      console.log(data);
-                      setCurrentPageIndex(data.selected);
-                    }}
-                    containerClassName="pagination"
-                    activeClassName="active"
-                    pageClassName="page"
-                    previousLabel="←"
-                    previousClassName="previous"
-                    nextLabel="→"
-                    nextClassName="next"
-                  />
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </main>
-      )}
+      </main>
     </Layout>
   );
 }
