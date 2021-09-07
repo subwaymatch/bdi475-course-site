035fb3f275169ca577496bfd58a6f00f4ad98a7b
Ye Joo Park <subwaymatch@gmail.com>
2021-09-07 12:28:11 -0500

    Replace links in home page with boxed items + geometric shapes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
 import { clickableVariants } from "animations/clickableVariants";
 import styles from "styles/pages/index.module.scss";
+import geometricImage1 from "public/images/geometric_01.png";
+import geometricImage2 from "public/images/geometric_02.png";
+import geometricImage3 from "public/images/geometric_03.png";
+import geometricImage4 from "public/images/geometric_04.png";
+import Image from "next/image";
 
 export default function MainPage() {
   return (
@@ -16,68 +21,87 @@ export default function MainPage() {
               </Col>
             </Row>
           </div>
-
-          <div className={styles.linksSection}>
-            <Row>
-              <Col>
-                <h2 className="sectionTitle">
-                  Links <span className="blue accent" />
-                </h2>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col md={4}>
-                <div className={styles.displayBox}>
-                  <span className="label yellow">Office Hours</span>
-                  <motion.div
-                    variants={clickableVariants}
-                    whileHover="hover"
-                    whileTap="tap"
-                    className={styles.text}
-                  >
-                    <a href="https://illinois.zoom.us/j/88199992344?pwd=dTMvdnM3SHF4eE11NnBjZGJ6NGhMZz09">
-                      Zoom Link ⟶
-                    </a>
-                  </motion.div>
-                  <div>Every Wednesday 3-4 PM</div>
-                </div>
-              </Col>
-
-              <Col md={4}>
-                <div className={styles.displayBox}>
-                  <span className="label green">Lecture Recordings</span>
-                  <motion.div
-                    variants={clickableVariants}
-                    whileHover="hover"
-                    whileTap="tap"
-                    className={styles.text}
-                  >
-                    <a href="https://mediaspace.illinois.edu/channel/Intro+to+Data+Analytics+Apps+in+Business+%28BDI+475%29+Fall+2021/227356363">
-                      Mediaspace ⟶
-                    </a>
-                  </motion.div>
-                </div>
-              </Col>
-
-              <Col md={4}>
-                <div className={styles.displayBox}>
-                  <span className="label purple">Course Page</span>
-                  <motion.div
-                    variants={clickableVariants}
-                    whileHover="hover"
-                    whileTap="tap"
-                    className={styles.text}
-                  >
-                    <a href="https://canvas.illinois.edu/courses/14860/">
-                      Canvas ⟶
-                    </a>
-                  </motion.div>
-                </div>
-              </Col>
-            </Row>
-          </div>
         </Container>
+
+        <section className={styles.linksSection}>
+          <Container>
+            <Row>
+              <Col lg={6}>
+                <a
+                  href="https://illinois.zoom.us/j/88199992344?pwd=dTMvdnM3SHF4eE11NnBjZGJ6NGhMZz09"
+                  className={styles.linkItem}
+                >
+                  <div className={styles.linkText}>
+                    <span className={styles.title}>Zoom Link ⟶</span>
+                    <p className={styles.desc}>
+                      Office hours are Wednesdays 3-4 PM or by appointment.
+                    </p>
+                  </div>
+
+                  <div className={styles.linkImage}>
+                    <Image src={geometricImage1} width={160} height={138} />
+                  </div>
+                </a>
+              </Col>
+
+              <Col lg={6}>
+                <a
+                  href="https://mediaspace.illinois.edu/channel/Intro+to+Data+Analytics+Apps+in+Business+%28BDI+475%29+Fall+2021/227356363"
+                  className={styles.linkItem}
+                >
+                  <div className={styles.linkText}>
+                    <span className={styles.title}>Mediaspace ⟶</span>
+                    <p className={styles.desc}>
+                      Lecture recordings are uploaded to this channel.
+                    </p>
+                  </div>
+
+                  <div className={styles.linkImage}>
+                    <Image src={geometricImage2} width={147} height={160} />
+                  </div>
+                </a>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col lg={6}>
+                <a
+                  href="https://canvas.illinois.edu/courses/14860"
+                  className={styles.linkItem}
+                >
+                  <div className={styles.linkText}>
+                    <span className={styles.title}>Canvas ⟶</span>
+                    <p className={styles.desc}>
+                      View announcements, grades, and all course materials
+                    </p>
+                  </div>
+
+                  <div className={styles.linkImage}>
+                    <Image src={geometricImage3} width={130} height={160} />
+                  </div>
+                </a>
+              </Col>
+
+              <Col lg={6}>
+                <a
+                  href="https://www.datacamp.com/groups/shared_links/879a294f30aff1d0a8e33b431196f132c4c36ff01508779908c2a36164218b08"
+                  className={styles.linkItem}
+                >
+                  <div className={styles.linkText}>
+                    <span className={styles.title}>DataCamp Invitation ⟶</span>
+                    <p className={styles.desc}>
+                      Practice outside the classroom on a wide range of topics.
+                    </p>
+                  </div>
+
+                  <div className={styles.linkImage}>
+                    <Image src={geometricImage4} width={125} height={160} />
+                  </div>
+                </a>
+              </Col>
+            </Row>
+          </Container>
+        </section>
       </main>
     </Layout>
   );
