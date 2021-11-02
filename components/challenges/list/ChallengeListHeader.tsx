1d97ff4bd6afa0b46a3147a4bd8d1e9a9a027352
Ye Joo Park <subwaymatch@gmail.com>
2021-11-02 18:18:49 -0500

    Style create button

--- components/challenges/list/ChallengeListHeader.tsx
+++ components/challenges/list/ChallengeListHeader.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import styles from "./ChallengeListHeader.module.scss";
-import Button from "components/ui/Button";
-import { ColorTheme } from "types/color-theme";
+import clsx from "clsx";
+import { CgAddR } from "react-icons/cg";
+import { AiOutlineNumber } from "react-icons/ai";
+import Tippy from "@tippyjs/react";
+import { IoCreateOutline } from "react-icons/io5";
+import { MdOutlineAddCircle } from "react-icons/md";
 
 interface IChallengeListHeaderProps {
   create: () => void;
@@ -15,20 +19,27 @@ export default function ChallengeListHeader({
 }: IChallengeListHeaderProps) {
   return (
     <div className={styles.listHeader}>
-      <Row className="align-items-center">
-        <Col md={4}>
-          <Button
-            className={styles.createButton}
-            onClick={create}
-            label="+ Create"
-            tooltip="Create a new Python Challenge"
-            colorTheme={ColorTheme.Black}
-            fullWidth
-          />
+      <Row className={clsx("align-items-center", "g-0")}>
+        <Col md={6}>
+          <Tippy
+            key="create-button"
+            content="Create a new challenge"
+            className="tippy"
+            placement="bottom"
+            theme="light"
+          >
+            <div className={styles.createButton} onClick={create}>
+              <MdOutlineAddCircle className={styles.reactIcon} />
+              <span className={styles.label}>Create</span>
+            </div>
+          </Tippy>
         </Col>
 
-        <Col md={4}>
-          <div className={styles.pageDisplay}>Page {currentPage + 1}</div>
+        <Col md={6}>
+          <div className={styles.pageDisplay}>
+            <AiOutlineNumber className={styles.reactIcon} />
+            <span>Page {currentPage + 1}</span>
+          </div>
         </Col>
       </Row>
     </div>
