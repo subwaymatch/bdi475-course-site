6d5f61d27b255fe699ce7aff4cb2443f07f8234c
Ye Joo Park <subwaymatch@gmail.com>
2021-12-28 15:33:36 -0600

    Install supabase-js@1.29.1, fix next config to render tsx files as pages

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withMDX = require("@next/mdx")({
 });
 
 module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "md", "mdx"],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   webpack: (config, { isServer, webpack }) => {
     console.log(`Webpack version: ${webpack.version}`);
 
