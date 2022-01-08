66a9f60ff7ef8d862d540191cd1021726988d8bc
Ye Joo Park <subwaymatch@gmail.com>
2022-01-08 08:30:53 -0600

    Inter font optimzation

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
