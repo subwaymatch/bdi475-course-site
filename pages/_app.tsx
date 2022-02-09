89b4d282041f3ddfc610e0299e791cd200f0f696
Ye Joo Park <subwaymatch@gmail.com>
2022-02-09 11:14:29 -0600

    Apply global MUI theme to match existing palettes/typography

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,24 +9,62 @@ import "styles/toastify.custom.scss";
 import PythonRuntimeProvider from "lib/pyodide/PythonRuntimeProvider";
 import { UserContextProvider } from "context/UserContext";
 import { supabaseClient } from "lib/supabase/supabaseClient";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import sassVariables from "styles/colors.module.scss";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+
+const muiTheme = createTheme({
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+  },
+  palette: {
+    primary: {
+      contrastText: sassVariables.white,
+      main: sassVariables.purple,
+    },
+    secondary: {
+      contrastText: sassVariables.white,
+      main: sassVariables.green,
+    },
+    error: {
+      contrastText: sassVariables.white,
+      main: sassVariables.red,
+    },
+    warning: {
+      contrastText: sassVariables.white,
+      main: sassVariables.orange,
+    },
+    info: {
+      contrastText: sassVariables.white,
+      main: sassVariables.blue,
+    },
+    success: {
+      contrastText: sassVariables.white,
+      main: sassVariables.green,
+    },
+  },
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <UserContextProvider supabaseClient={supabaseClient}>
       <PythonRuntimeProvider>
-        <ToastContainer
-          position="top-center"
-          theme="colored"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <Component {...pageProps} />
+        <ThemeProvider theme={muiTheme}>
+          <ToastContainer
+            position="top-center"
+            theme="colored"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </PythonRuntimeProvider>
     </UserContextProvider>
   );
