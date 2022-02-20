938330e4af8ca353923662194aeed7dea88d6f88
Ye Joo Park <subwaymatch@gmail.com>
2022-02-20 04:40:20 -0600

    Adjust MUI base font size/spacing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,8 +18,18 @@ import PythonRuntimeProvider from "lib/pyodide/PythonRuntimeProvider";
 const muiTheme = createTheme({
   typography: {
     fontFamily: ["Inter", "sans-serif"].join(","),
+    fontSize: 16,
     button: {
       textTransform: "none",
+      letterSpacing: "-0.01rem",
+    },
+    body1: {
+      fontSize: "1.2rem",
+      letterSpacing: "-0.015rem",
+    },
+    body2: {
+      fontSize: "1.1rem",
+      letterSpacing: "-0.015rem",
     },
   },
   palette: {
