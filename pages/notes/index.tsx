d13814b1db373f0b470f3cf80db460f7955d2d0e
Ye Joo Park <subwaymatch@gmail.com>
2022-03-09 23:51:59 -0600

    Add hypothesis testing note

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -234,6 +234,21 @@ export default function NotesPage() {
               title="Cell Operations, Advanced Exercises"
               colorTheme={ColorTheme.Purple}
             />
+
+            <NoteItem
+              href="https://nbviewer.org/github/bdi475/notebooks/blob/3699e186389ba9035794008559f2a25fec1b5638/L15-hypothesis-testing-vocareum-SOLUTION.ipynb"
+              thumbnail={
+                <Image
+                  src="/images/notes/pandas-gray-bg.png"
+                  width={900}
+                  height={450}
+                  alt=""
+                />
+              }
+              date="2021-03-08"
+              title="Hypothesis Testing"
+              colorTheme={ColorTheme.Purple}
+            />
           </Row>
 
           {/* <Row>
