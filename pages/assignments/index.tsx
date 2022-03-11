79addc2811f948d3d81c95ec84ca34a2fd901e7c
Ye Joo Park <subwaymatch@gmail.com>
2022-03-11 03:26:59 -0600

    Update starbucks app case thumnail

--- pages/assignments/index.tsx
+++ pages/assignments/index.tsx
@@ -320,7 +320,7 @@ export default function AssignmentsPage() {
                   />
                 }
               />
-            </Col>*/}
+            </Col> */}
           </Row>
         </Container>
       </main>
