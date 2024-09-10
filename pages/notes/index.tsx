1ddb30e6e7e6a5f9c8329d563b213d7ea0b8c5f7
Ye Joo Park <subwaymatch@gmail.com>
2024-09-09 20:49:20 -0500

    Release L5 note

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -118,7 +118,7 @@ export default function NotesPage() {
               date="2024-09-10"
               title="Collections and Strings"
               colorTheme={ColorTheme.Purple}
-              show={false}
+              show={true}
             />
 
             <NoteItem
