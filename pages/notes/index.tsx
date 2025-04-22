60fbb8d5eee5f9c85c54a452ad3ef6d4c7e4be9e
Ye Joo Park <subwaymatch@gmail.com>
2025-04-22 12:13:22 -0500

    Release L25 note

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -419,7 +419,7 @@ export default function NotesPage() {
               date="2025-04-22"
               title="Advanced Tableau Visualizations"
               colorTheme={ColorTheme.Blue}
-              show={false}
+              show={true}
             />
           </Row>
 
